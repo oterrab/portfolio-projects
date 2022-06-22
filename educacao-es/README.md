2f554081dfac4861fc76b1675a844c67bc69d716
Lucas Passos Barreto <59098085+lucaspassosbarreto@users.noreply.github.com>
2022-06-21 22:55:41 -0300

    generated new table and visualization

--- educacao-es/README.md
+++ educacao-es/README.md
@@ -7,6 +7,6 @@ This is an analisys of schools per counties aggregating them by their unique sch
 ![Schools per Counties](output/images/escolaxmunicipios.png)
 
 
-Also did a count of unique schools shifts. Some schools offer more than one shift and in total the file have 97027 observations. So I had to isolate unique shifts grouped by INEP.
+Also did a count of unique schools shifts. Some schools offer more than one shift and in total the file have 97027 shift  observations over several years (2008-2022). So I isolated unique shifts grouped by INEP for the year of 2022.
 
 ![Turnos](output/images/turno.png)
--- educacao-es/inep-es.py
+++ educacao-es/inep-es.py
@@ -25,3 +25,33 @@ os.chdir(r'/Users/lucaspb/git-repositories/portifolio-projects')
 #Importing Data
 
 data_inep  = pd.read_excel('educacao-es/input/inep-resultado_final_anexo_1_.xlsx', skiprows=4872, nrows=474)
+data_inep.columns = ['c0','infantil creche parcial', 'infantil creche integral', 'infantil preescola parcial', 
+                     'infantil preescola integral', 'fundamental iniciais parcial', 'fundamental iniciais integral',
+                     'fundamental finais parcial', 'fundamental finais integral', 'medio Parcial', 'medio integral', 
+                     'eja fundamental', 'eja medio']
+
+# Generating new table with students from middle school from each city
+        
+df_medio = pd.DataFrame(columns=['cidades','Estadual', 'Estadual Urbana', 'Estadual Rural','Municipal', 'Municipal Urbana', 'Municipal Rural', 'Estadual e Municipal'])
+for index, row in data_inep.iterrows():
+    if data_inep.loc[index, 'c0'].isupper() == True:
+        df_medio.loc[index, 'cidades'] = data_inep.loc[index, 'c0']
+        df_medio.loc[index, 'Estadual'] = data_inep.loc[index+1, 'medio Parcial'] + data_inep.loc[index+1, 'medio integral'] + data_inep.loc[index+2, 'medio Parcial'] + data_inep.loc[index+2, 'medio integral']    
+        df_medio.loc[index, 'Estadual Urbana'] = data_inep.loc[index+1, 'medio Parcial'] + data_inep.loc[index+1, 'medio integral']
+        df_medio.loc[index, 'Estadual Rural'] = data_inep.loc[index+2, 'medio Parcial'] + data_inep.loc[index+2, 'medio integral']    
+        df_medio.loc[index, 'Municipal'] = data_inep.loc[index+3, 'medio Parcial'] + data_inep.loc[index+3, 'medio integral'] + data_inep.loc[index+4, 'medio Parcial'] + data_inep.loc[index+4, 'medio integral']
+        df_medio.loc[index, 'Municipal Urbana'] = data_inep.loc[index+3, 'medio Parcial'] + data_inep.loc[index+3, 'medio integral']
+        df_medio.loc[index, 'Municipal Rural'] = data_inep.loc[index+4, 'medio Parcial'] + data_inep.loc[index+4, 'medio integral']
+        df_medio.loc[index, 'Estadual e Municipal'] = data_inep.loc[index+5, 'medio Parcial'] + data_inep.loc[index+5, 'medio integral']       
+     
+## Plotting
+
+plt.figure(figsize=(15,18))
+sns.barplot(data=df_medio.iloc[1:], x='cidades', y='Estadual e Municipal')
+plt.xticks(rotation = 90)
+
+plt.savefig('educacao-es/output/images/medioalunosxcidade.png', quality=95, dpi=600)
+         
+        
+
+        
