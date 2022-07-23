3608c89a7aa1641a856118019facd9a1568049e0
Lucas Passos Barreto <59098085+lucaspassosbarreto@users.noreply.github.com>
2022-07-22 23:21:49 -0300

    new query

--- sql/oracle-sql-course/transposing.sql
+++ sql/oracle-sql-course/transposing.sql
@@ -4,5 +4,18 @@ from employee;
 select *
 from department;
 
--- Query to list employees data with the following changes:
+-- Write a query that returns a single row that has one column for each of the monthly budgets of the departments ACCOUNTING, MARKETING, and INFORMATION TECHNOLOGY. The column titles for the budgets must be the names of the departments.
 
+select *
+from 
+(
+select name, monthly_budget
+from department
+)
+pivot(
+max(monthly_budget) for name in ('ACCOUNTING', 'MARKETING','INFORMATION TECHNOLOGY')
+);
+
+
+
+-- Write a query to generate a list of employees who work in the Accounting and Marketing departments. The report must include the name, birthdate, and hire_date, but each employee must appear in the report 2 times, and the birth date and hire date must appear in the same column but in different rows. This column must be called “date_value” and there must be an additional column that explains the kind of date that is included in the date_value column. This additional column must be called “date_type” and will contain either “Date of Birth” or “Date of Hiring”.
