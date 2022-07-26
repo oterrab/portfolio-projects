9343a4bc0a6660a9f025b2fd8d429f880489636d
Lucas Passos Barreto <59098085+lucaspassosbarreto@users.noreply.github.com>
2022-07-26 12:37:53 -0300

    new queries

---
+++ sql/oracle-sql-course/group_operations.sql
@@ -0,0 +1,16 @@
+/*
+Write a query to get the number of employees in the Accounting department, the total sum of their salaries, and the average salary. The average must appear 2 times in the results, one of them must be calculated using the AVG function, and one without using the AVG function. Please add column aliases to make it easy to understand the columns in the result.
+*/
+
+
+/*
+Write a query to list the different bonuses from the employee table, along with the number of employees that earn that bonus, and the greatest salary for employees in that group. Please include only employees who were born before 1995.
+*/
+
+
+/*
+Write a query to list the minimum and maximum salaries and also the bonus average per department from the employee table, but please don’t include employees who don’t have a value defined for their bonus.
+
+Also, please show in the results only departments whose smallest salary is less than 2000 or their highest salary is greater than 4000. The results must be displayed in descending order by the minimum salary.
+*/
+
---
+++ sql/oracle-sql-course/operators.sql
@@ -0,0 +1,35 @@
+/*
+Write a query to get the list of employees whose name includes the letter “O” 2 times, but not contiguously, so if there was a name “JOHN DOE”, it should be returned, but a row with name “JIM BROOKS” should not, because the 2 Os are contiguous.
+
+Note: Since at this point we still don’t have the necessary knowledge to count the number of ‘O’s that appear in a string, assume that no name will contain more than 2 Os.
+*/
+
+
+/*
+Write a query to get the list of departments whose monthly budget is greater than 15000 and its name includes a “G” or starts with an “H”, sorted by the department id in descending order.
+*/
+
+
+/*
+Write a query to list all employees of the Information Technology and Human Resources departments who earn 3000 or more but not more than 5000. Please include only employees who were born between 1970 and 1990.
+*/
+
+/*
+Write a query to list all employees who were born before 01-jan-1980 or after 01-jan-1995 and earn more than 2000 a month, and whose name does not start or end with an “N”.
+*/
+
+/*
+Warning: As mentioned in the lesson, the ability to use substitution variables is a feature of SQL Developer (and some other client tools), but this feature is not implemented by Live SQL and Apex, so if you try to run statements with substitution variables there you will get errors.
+*/
+
+/*
+Write a query to list employees who were hired between a certain range of dates (for example, between January 1st, 2011, and December 15th, 2014). The query must be written in a way that allows the user to use a different date range without needing to modify the query.
+*/
+
+/*
+Write a query to list employees who work in a given department. The query must prompt the user to enter a value, and then return only employees whose salary is equal to the value entered, or equal to the half of the value entered or equal to a third of the value entered by the user.
+
+The query must also prompt for the id of the department that the user wants to list.
+
+Your script must include the command necessary to erase the value entered for the salary so that if the query is executed again, the user is prompted for the salary value again.
+*/
