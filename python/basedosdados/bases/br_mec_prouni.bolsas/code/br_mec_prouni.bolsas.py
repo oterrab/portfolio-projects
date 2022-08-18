87734ee06ae380fe8eb17828f88d7103fc50c43d
Lucas Passos Barreto <59098085+lucaspassosbarreto@users.noreply.github.com>
2022-08-18 19:26:27 -0300

    basedosdados

--- python/basedosdados/bases/br_mec_prouni.bolsas/code/br_mec_prouni.bolsas.py
+++ python/basedosdados/bases/br_mec_prouni.bolsas/code/br_mec_prouni.bolsas.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import glob
 import time
-import unidecode
+#from unidecode import unidecode
 
 # Setting working directory
 
@@ -34,11 +34,6 @@ rename = {'ANO_CONCESSAO_BOLSA':'ano',
           'NOME_TURNO_CURSO_BOLSA':'turno_curso',
           'TIPO_BOLSA':'tipo_bolsa',
           'MODALIDADE_ENSINO_BOLSA':'modalidade_ensino' }
-
-
-ordem = ["ano", "sigla_uf", "id_municipio", "cpf", "sexo", "raca_cor", "data_nascimento", 
-         "beneficiario_deficiente", "id_ies", "curso", "turno_curso", "tipo_bolsa", "modalidade_ensino"]
-
 
 ordem2020 = ['ano', 'sigla_uf', 'id_municipio', 'cpf', 'sexo', 'raca_cor', 'data_nascimento',
          'beneficiario_deficiente', 'id_ies', 'campus', 'id_municipio_ies', 'curso', 'turno_curso', 'tipo_bolsa', 'modalidade_ensino']
@@ -72,7 +67,7 @@ del(path)
 dfs = [pd.read_csv(file, sep=";", encoding='latin-1') for file in csv_files]
 
 ## this could be made into one bit dataframe, but I've decided not to go on this route
-## prounidata = pd.concat(dfs,ignore_index=True)
+##prounidata = pd.concat(dfs,ignore_index=True)
 
 # Cleaning data
 for df in dfs:
@@ -80,31 +75,29 @@ for df in dfs:
     df.drop(['NOME_IES_BOLSA', 'REGIAO_BENEFICIARIO_BOLSA', 'REGIAO_BENEFICIARIO'], errors='ignore', axis=1, inplace=True)
     df['sexo'] = df['sexo'].str.upper().replace({'FEMININO':'F', 'MASCULINO':'M'})
     df['sexo'] = df['sexo'].replace({'F':'1', 'M':'2'})
-    df['raca_cor'] = df['raca_cor'].str.upper().replace({'AMARELA':'1', 'BRANCA':'2', 'INDÍGENA':'3', 'PARDA':'4', 'PRETA':'5', 'NÃO INFORMADA':'6', 'NÆO INFORMADA':'6'})
+    df['raca_cor'] = df['raca_cor'].str.upper().replace({'AMARELA':'1', 'BRANCA':'2', 'INDÍGENA':'3', 'PARDA':'4', 'PRETA':'5', 'NÃO INFORMADA':'6'})
     df['beneficiario_deficiente'] = df['beneficiario_deficiente'].str.upper().replace({'SIM':'1', 'NÃO':'0'})
     df['beneficiario_deficiente'] = df['beneficiario_deficiente'].str.upper().replace({'S':'1', 'N':'0'})
-    df['turno_curso'] = df['turno_curso'].str.upper().replace({'MATUTINO':'1', 'VESPERTINO':'2', 'NOTURNO':'3', 'INTEGRAL':'4', 'CURSO A DISTÂNCIA':'5', 'CURSO A DIST¶NCIA':'5'})
-    df['tipo_bolsa'] = df['tipo_bolsa'].str.upper().replace({'BOLSA INTEGRAL':'1', 'BOLSA PARCIAL 50%':'2'})
+    df['turno_curso'] = df['turno_curso'].str.upper().replace({'MATUTINO':'1', 'VESPERTINO':'2', 'NOTURNO':'3', 'INTEGRAL':'4', 'CURSO A DISTÂNCIA':'5'})
+    df['tipo_bolsa'] = df['tipo_bolsa'].str.upper().replace({'BOLSA INTEGRAL':'1', 'BOLSA PARCIAL 50%':'2', 'BOLSA COMPLEMENTAR 25%':'3'})
     df['tipo_bolsa'] = df['tipo_bolsa'].str.upper().replace({'INTEGRAL':'1', 'PARCIAL':'2'})
     df['modalidade_ensino'] = df['modalidade_ensino'].str.upper().replace({'PRESENCIAL':'1', 'EDUCAÇÃO A DISTÂNCIA':'2', 'EAD':'2'})
     df['data_nascimento'] = pd.to_datetime(df['data_nascimento'], errors='coerce')
     df['data_nascimento'] = df['data_nascimento'].dt.strftime('%Y-%m-%d')
 
-del(df)
+for df in dfs:
+    df['turno_curso'] = df['turno_curso'].str.upper().replace({'CURSO A DISTÃ¢NCIA':'5'})
+    df['turno_curso'] = df['turno_curso'].str.upper().replace({'CURSO A DISTÃ¢NCIA':'5'})
+    df['turno_curso'] = df['turno_curso'].str.upper().replace({'CURSO A DISTNCIA':'5'})
+    df['raca_cor'] = df['raca_cor'].str.upper().replace({'IND¡GENA':'3', 'NÆO INFORMADA':'6'})
+    df['raca_cor'] = df['raca_cor'].str.upper().replace({'INDÃ­GENA':'3', 'NÃ£O INFORMADA':'6'})
 
-# Reordering columns
-for i in range(len(dfs)):
-    ano = int(dfs[i]['ano'].iat[0])
-    if ano != 2020:
-        dfs[i] = dfs[i][ordem]
-    if ano == 2020:
-        dfs[i] = dfs[i][ordem2020]  
-        
-del(ano, i)
+
+del(df)
 
 # Setting to replace counties names for its unique id
 ## Importing dataframe with counties and unique ids
-id_mcp = pd.read_csv(r'/Users/lucaspb/git-repositories/portfolio-projects/python/basedosdados/assets/dir_municipio.csv', sep=',', dtype='string')
+id_mcp = pd.read_csv(r'/Users/lucaspb/git-repositories/portfolio-projects/python/basedosdados/assets/dir_municipio_uf.csv', sep=',', dtype='string')
 
 ## Normalizing names
 ### Upper Case
@@ -121,37 +114,68 @@ for df in dfs:
 del(ano)
 
 ### Removing accentuation
+####id_mcp['id_municipio'] = id_mcp['id_municipio'].apply(unidecode)
 cols = id_mcp.select_dtypes(include=[np.object]).columns
 id_mcp[cols] = id_mcp[cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
-id_mcp['f0_'] = id_mcp['f0_'].apply(lambda x: x.replace("‘", ' ').replace("’", ' ').replace("'", ' '))
+id_mcp['nome'] = id_mcp['nome'].apply(lambda x: x.replace("‘", ' ').replace("’", ' ').replace("'", ' ').replace('"', ' ').replace('`', ' ').replace('-', ' '))
+id_mcp['nome'] = id_mcp['nome'].apply(lambda x: x.replace("SANT ANA DO LIVRAMENTO", 'SANTANA DO LIVRAMENTO'))
+
 
 for df in dfs:
     ano = int(df['ano'][0])
     if ano != 2020:
-        df['id_municipio'] = df['id_municipio'].apply(lambda x: x.encode('utf-8').decode('ascii', 'ignore') if type(x) == str else x)
+        cols = df.select_dtypes(include=[np.object]).columns
+        df[cols] = df[cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+        df['id_municipio'] = df['id_municipio'].apply(lambda x: x.replace("‘", ' ').replace("’", ' ').replace("'", ' ').replace('"', ' ').replace('`', ' ').replace('-', ' ') if type(x) == str else x)
     if ano == 2020: 
-        df['id_municipio'] = df['id_municipio'].apply(lambda x: x.encode('utf-8').decode('ascii', 'ignore') if type(x) == str else x)
-        df['id_municipio_ies'] = df['id_municipio_ies'].apply(lambda x: x.encode('utf-8').decode('ascii', 'ignore') if type(x) == str else x)
-        
-## Replacing counties names for its unique id 
-id_mcp = id_mcp.set_index('f0_').to_dict()['id_municipio']
+        cols = df.select_dtypes(include=[np.object]).columns
+        df[cols] = df[cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+        df['id_municipio'] = df['id_municipio'].apply(lambda x: x.replace("‘", ' ').replace("’", ' ').replace("'", ' ').replace('"', ' ').replace('`', ' ').replace('-', ' ') if type(x) == str else x)
+        df['id_municipio_ies'] = df['id_municipio_ies'].apply(lambda x: x.replace("‘", ' ').replace("’", ' ').replace("'", ' ').replace('"', ' ').replace('`', ' ').replace('-', ' ') if type(x) == str else x)
 
+
+for df in dfs:
+    ano = int(df['ano'][0])
+    if ano != 2020:
+        df['id_municipio'] = df['id_municipio'].apply(lambda x: x.replace("EMBU", 'EMBU DAS ARTES') if type(x) == str else x)
+    if ano == 2020: 
+        df['id_municipio'] = df['id_municipio'].apply(lambda x: x.replace("EMBU", 'EMBU DAS ARTES') if type(x) == str else x)
+        df['id_municipio_ies'] = df['id_municipio_ies'].apply(lambda x: x.replace("EMBU", 'EMBU DAS ARTES') if type(x) == str else x)
+
+
+for i in range(len(dfs)):
+     ano = int(dfs[i]['ano'].iat[0])
+     if ano != 2020:
+         dfs[i] = pd.merge(id_mcp, dfs[i],
+                    how='right', left_on=['sigla_uf', 'nome'], right_on=['sigla_uf', 'id_municipio'])
+     if ano == 2020:
+         dfs[i] = pd.merge(id_mcp, dfs[i],
+                    how='right', left_on=['sigla_uf', 'nome'], right_on=['sigla_uf', 'id_municipio'])
+         dfs[i] = pd.merge(id_mcp, dfs[i],
+                    how='right', left_on=['sigla_uf', 'nome'], right_on=['sigla_uf', 'id_municipio_ies'])
+
+
+# Reordering columns
 for i in range(len(dfs)):
     ano = int(dfs[i]['ano'].iat[0])
     if ano != 2020:
-        dfs[i] = dfs[i].replace({"id_municipio": id_mcp})
+        dfs[i].rename(columns={'id_municipio_x':'id_municipio'}, inplace=True)
+        dfs[i] = pd.concat([dfs[i],pd.DataFrame(columns=['campus', 'id_municipio_ies'])])
+        dfs[i] = dfs[i][ordem2020]
     if ano == 2020:
-        dfs[i] = dfs[i].replace({"id_municipio": id_mcp})
-        dfs[i] = dfs[i].replace({"id_municipio_ies": id_mcp})
+        dfs[i].rename(columns={'id_municipio_x':'id_municipio'}, inplace=True)
+        dfs[i].rename(columns={'id_municipio_x':'id_municipio'}, inplace=True)
+        dfs[i] = dfs[i][ordem2020]  
+        
 
 # Saving partitioned data into specific folders
 for df in dfs:
     ano = int(df['ano'][0])
     print("Particionando ano {}".format(ano))
-    time.sleep(2)
+    time.sleep(1)
     for uf in ufs:
         print("Particionando {} de {}".format(uf, ano))
-        time.sleep(2)
+        time.sleep(1)
         df_uf = df[df['sigla_uf'] == uf]
         df_uf.drop(['ano', 'sigla_uf'], axis=1, inplace=True)
-        exec("df_uf.to_csv('output/ano={}/sigla_uf={}/microdados.csv', index=False, encoding='utf-8', na_rep='')".format(ano, uf))
+        exec("df_uf.to_csv('output/ano={}/sigla_uf={}/microdados.csv', index=False, encoding='latin-1', na_rep='', float_format='%.0f')".format(ano, uf))
