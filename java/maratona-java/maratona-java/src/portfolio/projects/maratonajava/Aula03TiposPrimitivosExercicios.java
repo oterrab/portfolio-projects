f510698acf3f86d7828743bd73015a7418755bb8
Lucas Passos <l.passos@reply.com>
2023-10-10 10:01:12 -0300

    Tipos Primitivos Execicios

---
+++ java/maratona-java/maratona-java/src/portfolio/projects/maratonajava/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,15 @@
+package portfolio.projects.maratonajava;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String name = "Jiraya";
+        String address = "R. das Geléias";
+        double salary = 5698.25;
+        String dateincome ="10/10/2023";
+
+        String message = "Eu " + name + ", morando no endereço " + address + " confirmo que recebi o salário de " + salary + ", na data " + dateincome;
+
+        System.out.println("Eu " + name + ", morando no endereço " + address + " confirmo que recebi o salário de " + salary + ", na data " + dateincome);
+        System.out.println(message);
+    }
+}
