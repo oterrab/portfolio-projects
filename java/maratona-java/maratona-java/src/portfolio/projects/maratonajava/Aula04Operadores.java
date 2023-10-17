11cfd9b845d9bdc532eaed02caab86cc8739869e
Lucas Passos <l.passos@reply.com>
2023-10-17 09:49:29 -0300

    Aula 04

--- java/maratona-java/maratona-java/src/portfolio/projects/maratonajava/Aula04Operadores.java
+++ java/maratona-java/maratona-java/src/portfolio/projects/maratonajava/Aula04Operadores.java
@@ -1,5 +1,8 @@
 package portfolio.projects.maratonajava;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class Aula04Operadores {
     public static void main(String[] args) {
         int number01 = 10;
@@ -16,7 +19,29 @@ public class Aula04Operadores {
 
         // < > <= >= == != -- Restorna valores booleanos
         boolean isTenbiggerthanTwenty = 10 > 20;
-        System.out.println("isTenbiggerthanTwenty " + isTenbiggerthanTwenty );
+        System.out.println("isTenbiggerthanTwenty -> " + isTenbiggerthanTwenty );
 
+        // && (AND) || (OR) ! (NOT)
+        int age = 34;
+        float salary = 3500F;
+        boolean isLaw = age >30 && salary >= 4612;
+
+        System.out.println("isLaw -> " + isLaw);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000; // bonus = bonus + 1000;
+        System.out.println(bonus);
+
+        // ++ --
+        int count = 0;
+        count++; //count +=1;
+
+        System.out.println(count);
+
+        int count2 =0;
+        int count3 =0;
+        System.out.println(++count2); // primeiro incrementa e depois executa
+        System.out.println(count3++); // primeiro executa e depois incrementa
     }
 }
